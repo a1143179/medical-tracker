3db120ab08338f9548f424d96b4bcf0f71fe2005
ice <a1143179@gmail.com>
2025-07-17 21:57:53 +1000

    fix: manually add CreatedAt column to Records table in migration

--- backend/Migrations/20250717115529_AddCreatedAtToRecords.cs
+++ backend/Migrations/20250717115529_AddCreatedAtToRecords.cs
@@ -10,13 +10,20 @@ namespace backend.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Records",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(2000, 1, 1, 0, 0, 0, DateTimeKind.Utc));
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Records");
         }
     }
 }
