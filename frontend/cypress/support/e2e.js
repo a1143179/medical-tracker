8971f08044aa8cab61192fc520d631475964d35a
ice <a1143179@gmail.com>
2025-07-24 17:15:13 +1000

    test: use cy.intercept('**') with cy.task for request/response logging in CI

--- frontend/cypress/support/e2e.js
+++ frontend/cypress/support/e2e.js
@@ -1,7 +1,7 @@
 beforeEach(() => {
-  cy.intercept({ url: '**', middleware: true }, (req) => {
+  cy.intercept('**', (req) => {
     cy.task('log', `[CYPRESS][request] ${req.method} ${req.url} ${JSON.stringify(req.headers)} ${JSON.stringify(req.body)}`);
-    req.on('response', (res) => {
+    req.continue((res) => {
       cy.task('log', `[CYPRESS][response] ${res.statusCode} ${req.url} ${JSON.stringify(res.body)}`);
     });
   });
